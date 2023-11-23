8b434ca2604a0fcbd34debb139de0afdeee77078
Roisull <lusiro2001@students.ac.id>
2023-11-23 19:37:09 +0700

    progress 3 membuat contoh stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,41 @@ class Heading extends StatelessWidget {
     );
   }
 }
+
+class BiggerText extends StatefulWidget {
+
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          widget.text,
+          style: TextStyle(
+            fontSize: _textSize
+          ),
+        ),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+
+          onPressed: (){
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
