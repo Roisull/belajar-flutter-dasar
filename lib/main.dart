50793a45eb4e4a72758d0c1ed56a842463a90cb5
Roisull <lusiro2001@students.ac.id>
2023-11-23 19:35:56 +0700

    progress 2 membuat contoh stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hallo ngab"),
-        ),
+      home: const Scaffold(
         body: Center(
-          child: Text("Hello ngab-ngab kabeh"),
+          child: Heading(
+            text: "Hallo ngab",
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+
+  final String text;
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
